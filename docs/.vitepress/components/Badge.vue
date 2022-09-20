236180b1ea770bc38251b84d81bffd94151d9378
jonsam <jonsam.ng@foxmail.com>
2022-09-20 18:17:21 +0800

    feat: 🆕 support video and pdf

---
+++ docs/.vitepress/components/Badge.vue
@@ -0,0 +1,47 @@
+<template>
+	<span :class="['badge', type]" :style="styles">{{ text }}</span>
+</template>
+<script>
+export default {
+	name: "Badge",
+	functional: true,
+	props: {
+		type: {
+			type: String,
+			default: "tip",
+		},
+		text: String,
+		vertical: {
+			type: String,
+			default: "top",
+		},
+	},
+	computed: {
+		styles() {
+			return {
+				verticalAlign: this.vertical,
+			};
+		},
+	},
+};
+</script>
+
+<style lang="stylus" scoped>
+.badge
+  display inline-block
+  font-size 12px
+  height 18px
+  line-height 18px
+  border-radius 3px
+  padding 0 6px
+  color white
+  background-color #42b983
+  &.tip, &.green
+    background-color $badgeTipColor
+  &.error
+    background-color $badgeErrorColor
+  &.warning, &.warn, &.yellow
+    background-color $badgeWarningColor
+  & + &
+    margin-left 5px
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,24 @@
 import DefaultTheme from "vitepress/theme";
+import LastReadingPopup from "../components/LastReadingPopup.vue";
+import Badge from "../components/Badge.vue";
+import Badges from "../components/Badges.vue";
+import Pdf from "../components/Pdf.vue";
+import VideoPlayer from "../components/VideoPlayer.vue";
+import Vue3VideoPlayer from "@cloudgeek/vue3-video-player";
+import "@cloudgeek/vue3-video-player/dist/vue3-video-player.css";
 
 export default {
 	...DefaultTheme,
-	enhanceApp({ app, router, siteData }) {},
+	enhanceApp({ app, router, siteData }) {
+		app.component(LastReadingPopup.name, LastReadingPopup);
+		app.component(Badge.name, Badge);
+		app.component(Badges.name, Badges);
+		app.component(Pdf.name, Pdf);
+		app.component(VideoPlayer.name, VideoPlayer);
+		// see https://github.com/LarchLiu/vue3-video-player/
+		app.use(Vue3VideoPlayer, {
+			lang: "zh-CN",
+		});
+	},
 	setup() {},
 };
