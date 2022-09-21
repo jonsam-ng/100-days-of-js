47b5ef93687dbd581746c047c21642b0f676afae
jonsam <jonsam.ng@foxmail.com>
2022-09-21 11:18:15 +0800

    feat: 🆕 hide qrcode on mobile

--- docs/.vitepress/plugins/qrcode.js
+++ docs/.vitepress/plugins/qrcode.js
@@ -1,4 +1,5 @@
 import { nextTick, defineComponent, createApp } from "vue";
+import isMobile from "ismobilejs";
 
 // 判断是否绑定时间是否绑定成功
 let isMounted = false;
@@ -7,6 +8,8 @@ export default ({ app }) => ({
 	updated() {
 		const qrcode = app._context.components.Qrcode;
 		if (isMounted || !qrcode) return;
+		const isPhone = window ? isMobile(window.navigator).phone : true;
+		if (isPhone) return;
 		isMounted = true;
 		// Execute after waiting for dom to load
 		const navLink = document.querySelector(".VPNavBar .content");
